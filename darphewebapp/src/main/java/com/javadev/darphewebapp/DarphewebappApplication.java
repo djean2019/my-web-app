a8f2df504e63d67565d814f16386ae46b6ea5543
Darhyppo <darhyppo@gmail.com>
2020-09-26 20:21:48 -0400

    Startup

---
+++ darphewebapp/src/main/java/com/javadev/darphewebapp/DarphewebappApplication.java
@@ -0,0 +1,13 @@
+package com.javadev.darphewebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarphewebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DarphewebappApplication.class, args);
+	}
+
+}
---
+++ darphewebapp/src/test/java/com/javadev/darphewebapp/DarphewebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javadev.darphewebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DarphewebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
